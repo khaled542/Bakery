cd505e4bd1e144f2a57ed9e9a3b86843d3d347f0
khaled542 <64127531+khaled542@users.noreply.github.com>
2022-07-13 02:02:45 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+let aboutOffset=$(".about").offset().top;
+console.log(aboutOffset);
+
+
+$(window).scroll(function(){
+    if($(window).scrollTop() >=aboutOffset)
+    {
+        $(".navbar").css("backgroundColor","rgba(0,0,0,0.7)");
+        // $(".navbar").css("color","black")
+    }
+    else
+    {
+        $(".navbar").css("backgroundColor","transparent")
+    }
+})
